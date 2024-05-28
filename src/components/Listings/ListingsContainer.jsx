e7f3866314ff91d15d4bbd81e0bbe1b55e2bb538
Katiuska Alicea <katiuskaalicea@gmail.com>
2024-05-28 14:20:00 -0700

    Fix the protected route. I was getting the user state but not the actual user. I had to raech deeper into the object. Now the log out works, as the app is pointing to the correct user value.

--- src/components/Listings/ListingsContainer.jsx
+++ src/components/Listings/ListingsContainer.jsx
@@ -1,17 +1,24 @@
 import Listings from './Listings';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPosts } from '../../features/posts/postsSlice';
 
 const ListingsContainer = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts.posts);
+  const posts = useSelector((state) => state.posts);
+  console.log('ListingsContainer: posts:', posts);
+  const user = useSelector((state) => state.auth);
+  console.log('ListingsContainer: user: user.user', user.user); // user: user.user { _id: '60f7b3b3b3
 
   useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
+    if (!user.user) {
+      navigate('/login');
+    }
+  }, [user.user, navigate]);
 
-  return <Listings jobs={posts} />;
+  return <Listings posts={posts} />;
 };
 
 export default ListingsContainer;
